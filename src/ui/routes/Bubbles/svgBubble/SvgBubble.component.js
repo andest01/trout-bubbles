9164403157dd082e537971e22e580b5eaa324a24
Stuart Anderson <andest01@gmail.com>
2016-08-28 00:54:20 -0500

    More cleanup

--- src/ui/routes/Bubbles/svgBubble/SvgBubble.component.js
+++ src/ui/routes/Bubbles/svgBubble/SvgBubble.component.js
@@ -31,7 +31,6 @@ const SvgBubbleComponent = React.createClass({
   },
 
   componentWillMount () {
-    console.log('stream', this.props.streamPackage.stream)
     this.width = DIMENSIONS
     this.height = DIMENSIONS
 
@@ -42,18 +41,6 @@ const SvgBubbleComponent = React.createClass({
       .projection(this.projection)
       .pointRadius(1)
 
-    this.svgPath = this.pathGenerator(this.props.streamPackage.stream.geometry)
-
-    this.baseStreamOffset = (1000 * this.props.index) * ANIMATION_SCALE
-    this.baseStreamLength = (1000) * ANIMATION_SCALE
-    this.basePalOffset = (this.baseStreamOffset + this.baseStreamLength + 300 * ANIMATION_SCALE)
-    this.baseTroutSectionOffset = (this.baseStreamOffset + this.baseStreamLength + 600 * ANIMATION_SCALE)
-    this.baseRestrictionOffset = (this.baseStreamOffset + this.baseStreamLength + 900 * ANIMATION_SCALE)
-    this.baseAccessPointOffset = (this.baseStreamOffset + this.baseStreamLength + 1200 * ANIMATION_SCALE)
-
-    this.palSectionSpeed = 900 * ANIMATION_SCALE
-    this.troutSectionSpeed = (800 * ANIMATION_SCALE / Math.max(this.props.streamPackage.sections.length, 1))
-    this.accessPointSpeed = (1600 * ANIMATION_SCALE / Math.max(this.props.streamPackage.accessPoints.length, 1))
     this.layout = {
       width: this.width,
       height: this.height,
@@ -78,10 +65,7 @@ const SvgBubbleComponent = React.createClass({
         latitude: accessPoint.properties.centroid_latitude,
         longitude: accessPoint.properties.centroid_longitude
       }
-      let isBoring = accessPoint.properties.is_over_trout_stream === 0
-      let accessClass = isBoring
-        ? classes.accessPoint
-        : classes.boringAccessPoint
+      let accessClass = classes.accessPoint
       let timing = {
         offset: this.baseAccessPointOffset + accessPointsIndex * this.accessPointSpeed,
         length: 20
@@ -105,10 +89,7 @@ const SvgBubbleComponent = React.createClass({
         latitude: tributary.properties.centroid_latitude,
         longitude: tributary.properties.centroid_longitude
       }
-      let isBoring = tributary.properties.is_over_trout_stream === 0
-      let accessClass = isBoring
-        ? classes.accessPoint
-        : classes.boringAccessPoint
+      let accessClass = classes.accessPoint
       let timing = {
         offset: this.baseAccessPointOffset + accessPointsIndex * this.accessPointSpeed,
         length: 20
@@ -123,69 +104,6 @@ const SvgBubbleComponent = React.createClass({
         labelText={tributary.properties.streamData.stream.properties.name}
         layout={this.layout} />
     })
-  },
-
-  renderPalRings () {
-    return this.props.streamPackage.palSections.map((pal, palIndex) => {
-      let streamLength = this.props.streamPackage.stream.properties.length_mi
-      let itemOffset = ((streamLength - pal.properties.stop) / streamLength) * this.palSectionSpeed
-      let offset = this.basePalOffset + itemOffset
-      return (<RingSectionComponent
-        timing={{offset, length: this.baseStreamLength}}
-        cssName={classes.pal}
-        key={pal.properties.id}
-        layout={this.layout}
-        length={this.props.streamPackage.stream.properties.length_mi}
-        start={pal.properties.start}
-        stop={pal.properties.stop} />)
-    })
-  },
-
-  renderSectionRings () {
-    return this.props.streamPackage.sections.map((section, sectionIndex) => {
-      let streamLength = this.props.streamPackage.stream.properties.length_mi
-      let itemOffset = ((streamLength - section.properties.stop) / streamLength) * this.troutSectionSpeed
-      let offset = this.baseTroutSectionOffset + itemOffset
-      return (<RingSectionComponent
-        timing={{offset, length: this.baseStreamLength}}
-        cssName={classes.section}
-        key={section.properties.gid}
-        layout={this.layout}
-        length={this.props.streamPackage.stream.properties.length_mi}
-        start={section.properties.start}
-        stop={section.properties.stop} />)
-    })
-  },
-
-  renderRestrictionRings () {
-    return this.props.streamPackage.restrictions.map((restriction, restrictionIndex) => {
-      let streamLength = this.props.streamPackage.stream.properties.length_mi
-      let itemOffset = ((streamLength - restriction.properties.stop) / streamLength) * this.troutSectionSpeed
-      let offset = this.baseTroutSectionOffset + itemOffset
-      let className = restriction.properties.restriction_id === FISH_SANCTUARY_ID
-        ? classes.fishSanctuary
-        : classes.restriction
-      return (<RingSectionComponent
-        timing={{offset, length: this.baseStreamLength}}
-        cssName={className}
-        key={restriction.properties.gid}
-        layout={this.layout}
-        length={this.props.streamPackage.stream.properties.length_mi}
-        start={restriction.properties.start}
-        stop={restriction.properties.stop} />)
-    })
-  },
-
-  renderStreamRing () {
-    // return this.props.streamPackage.stream
-    let streamLength = this.props.streamPackage.stream.properties.length_mi
-    return (<RingSectionComponent
-      timing={{offset: this.baseStreamOffset, length: this.baseStreamLength}}
-      cssName={classes.stream}
-      layout={this.layout}
-      length={streamLength}
-      start={0}
-      stop={streamLength} />)
   },
 
   render () {
@@ -213,35 +131,7 @@ const SvgBubbleComponent = React.createClass({
               index={this.props.index}
               layout={this.layout} />
           </g>
-          <g id='ring'>
-            <g id='ring-pal'>
-            {
-              this.renderPalRings()
-            }
-            </g>
-
-            <g id='ring-stream'>
-            {
-              this.renderStreamRing()
-            }
-            </g>
-
-            <g id='ring-sections'>
-            {
-              this.renderSectionRings()
-            }
-            </g>
-            <g id='ring-restrictions'>
-            {
-              this.renderRestrictionRings()
-            }
-            </g>
-            <g id='ring-axis'>
-            {
-              this.renderOuterCircleAxis()
-            }
-            </g>
-          </g>
+          {this.renderOuterCircleAxis()}
           <g id='waypoints'>
             <g id='access-points'>
               {this.renderAccessPoints()}
--- src/ui/routes/Bubbles/svgBubble/ring/Ring.component.js
+++ src/ui/routes/Bubbles/svgBubble/ring/Ring.component.js
@@ -1,8 +1,10 @@
 import React, { PropTypes } from 'react'
-import classes from '../../Bubble.scss'
-import ringClasses from './Ring.scss'
 import RingAxisComponent from './RingAxis.component'
 import RingSectionComponent from './RingSection.component'
+
+import classes from '../SvgBubble.scss'
+const FISH_SANCTUARY_ID = 7
+const ANIMATION_SCALE = 2.0
 
 const RingComponent = React.createClass({
   propTypes: {
@@ -25,14 +27,123 @@ const RingComponent = React.createClass({
     })
   },
 
-  render () {
+  componentWillMount () {
+    this.baseStreamOffset = (1000 * this.props.index) * ANIMATION_SCALE
+    this.baseStreamLength = (1000) * ANIMATION_SCALE
+    this.basePalOffset = (this.baseStreamOffset + this.baseStreamLength + 300 * ANIMATION_SCALE)
+    this.baseTroutSectionOffset = (this.baseStreamOffset + this.baseStreamLength + 600 * ANIMATION_SCALE)
+    this.baseRestrictionOffset = (this.baseStreamOffset + this.baseStreamLength + 900 * ANIMATION_SCALE)
+    this.baseAccessPointOffset = (this.baseStreamOffset + this.baseStreamLength + 1200 * ANIMATION_SCALE)
+
+    this.palSectionSpeed = 900 * ANIMATION_SCALE
+    this.troutSectionSpeed = (800 * ANIMATION_SCALE / Math.max(this.props.streamPackage.sections.length, 1))
+    this.accessPointSpeed = (1600 * ANIMATION_SCALE / Math.max(this.props.streamPackage.accessPoints.length, 1))
+  },
+
+  renderPalRings () {
+    return this.props.streamPackage.palSections.map((pal, palIndex) => {
+      let streamLength = this.props.streamPackage.stream.properties.length_mi
+      let itemOffset = ((streamLength - pal.properties.stop) / streamLength) * this.palSectionSpeed
+      let offset = this.basePalOffset + itemOffset
+      return (<RingSectionComponent
+        timing={{offset, length: this.baseStreamLength}}
+        cssName={classes.pal}
+        key={pal.properties.id}
+        layout={this.props.layout}
+        length={this.props.streamPackage.stream.properties.length_mi}
+        start={pal.properties.start}
+        stop={pal.properties.stop} />)
+    })
+  },
+
+  renderSectionRings () {
+    return this.props.streamPackage.sections.map((section, sectionIndex) => {
+      let streamLength = this.props.streamPackage.stream.properties.length_mi
+      let itemOffset = ((streamLength - section.properties.stop) / streamLength) * this.troutSectionSpeed
+      let offset = this.baseTroutSectionOffset + itemOffset
+      return (<RingSectionComponent
+        timing={{offset, length: this.baseStreamLength}}
+        cssName={classes.section}
+        key={section.properties.gid}
+        layout={this.props.layout}
+        length={this.props.streamPackage.stream.properties.length_mi}
+        start={section.properties.start}
+        stop={section.properties.stop} />)
+    })
+  },
+
+  renderRestrictionRings () {
+    return this.props.streamPackage.restrictions.map((restriction, restrictionIndex) => {
+      let streamLength = this.props.streamPackage.stream.properties.length_mi
+      let itemOffset = ((streamLength - restriction.properties.stop) / streamLength) * this.troutSectionSpeed
+      let offset = this.baseTroutSectionOffset + itemOffset
+      let className = restriction.properties.restriction_id === FISH_SANCTUARY_ID
+        ? classes.fishSanctuary
+        : classes.restriction
+      return (<RingSectionComponent
+        timing={{offset, length: this.baseStreamLength}}
+        cssName={className}
+        key={restriction.properties.gid}
+        layout={this.props.layout}
+        length={this.props.streamPackage.stream.properties.length_mi}
+        start={restriction.properties.start}
+        stop={restriction.properties.stop} />)
+    })
+  },
+
+  renderStreamRing () {
+    // return this.props.streamPackage.stream
+    let streamLength = this.props.streamPackage.stream.properties.length_mi
+    return (<RingSectionComponent
+      timing={{offset: this.baseStreamOffset, length: this.baseStreamLength}}
+      cssName={classes.stream}
+      layout={this.props.layout}
+      length={streamLength}
+      start={0}
+      stop={streamLength} />)
+  },
+
+  renderRingAxis () {
     let length = this.props.streamPackage.stream.properties.length_mi
     let index = 0
-
-    return (<RingAxisComponent 
+    return (<RingAxisComponent
       length={length}
       index={index}
       layout={this.props.layout} />)
+  },
+
+  render () {
+    return (
+      <g id='ring'>
+        <g id='ring-pal'>
+        {
+          this.renderPalRings()
+        }
+        </g>
+
+        <g id='ring-stream'>
+        {
+          this.renderStreamRing()
+        }
+        </g>
+
+        <g id='ring-sections'>
+        {
+          this.renderSectionRings()
+        }
+        </g>
+        <g id='ring-restrictions'>
+        {
+          this.renderRestrictionRings()
+        }
+        </g>
+        <g id='ring-axis'>
+        {
+          this.renderRingAxis()
+        }
+        </g>
+      </g>
+      )
   }
 })
 
